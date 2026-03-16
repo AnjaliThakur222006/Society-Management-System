08c8452323153c243ba78241e28f5f47f58fd76d
Anjali Thakur <anjali@gmail.com>
2026-03-16 12:24:46 +0530

    Production deploy: Procfile + gunicorn + clean requirements

--- SMS-1/app.py
+++ SMS-1/app.py
@@ -4690,4 +4690,4 @@ def tenant_delivery_request():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=4000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 4000)))
